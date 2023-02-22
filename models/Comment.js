1867241ffa0daa6092ce8e4b6782d27ba5d8bec8
zzurawski <116835382+zzurawski@users.noreply.github.com>
2023-02-21 21:03:58 -0500

    made comment model

---
+++ models/Comment.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        user_name: {
+            allowNull: false,
+            type: DataTypes.STRING,
+            references: {
+                model: 'User',
+                key: 'username'
+            }
+        },
+        body: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+);
+
+module.exports = Comment
