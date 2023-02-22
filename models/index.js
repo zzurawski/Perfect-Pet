8adbe40b66014158def82d848f1648d368f05fc5
zzurawski <116835382+zzurawski@users.noreply.github.com>
2023-02-21 21:24:36 -0500

    comments of relationships

--- models/index.js
+++ models/index.js
@@ -2,21 +2,26 @@ const Pet = require('./pets');
 const User = require('./user');
 const Comment = require('./Comment');
 
+//user has ability to write multiple comments
 User.hasMany(Comment, {
     foreignKey: 'comment_id',
 });
 
+//the seller of the pet
 Pet.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
+//user can sell multiple pets
 User.hasMany(Pet, {
     foreignKey: 'pet_id'
 });
 
+//under pet post will be able to have multiple comments
 Pet.hasMany(Comment, {
     foreignKey: 'comment_id',
 });
+
 
 module.exports = {
     Pet,
