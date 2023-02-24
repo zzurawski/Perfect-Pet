ace9b4a801094a9da26b1f70eb06b5435debafe2
ScrimmyFS <dscrimshaw12345@gmail.com>
2023-02-23 19:31:10 -0500

    Server works now with Seq

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const path = require("path");
 const express = require("express");
-// const session = require("express-session");
+const session = require("express-session");
 const exphbs = require("express-handlebars");
 
-// const routes = require("./controllers");
+const routes = require("./controllers");
 
-// const sequelize = require("./config/connection");
-// const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -17,17 +17,17 @@ const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({});
 
-// const sess = {
-//   secret: process.env.SECRET,
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize,
-//   }),
-// };
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
-// app.use(session(sess));
+app.use(session(sess));
 
 
 
@@ -43,9 +43,9 @@ app.use(require("./controllers/Opener"));
 
 
 
-// sequelize.sync({ force: false }).then(() => {
-//   app.listen(PORT, () => console.log("Now listening"));
-// });
-app.listen(PORT, () => {
-  console.log("Server listening on: http://localhost:" + PORT);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
 });
+// app.listen(PORT, () => {
+//   console.log("Server listening on: http://localhost:" + PORT);
+// });
