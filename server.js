f9382ed1e50c9aa83b73f243008a180cb98cd622
ScrimmyFS <dscrimshaw12345@gmail.com>
2023-02-23 19:16:37 -0500

    Turned server on

--- server.js
+++ server.js
@@ -1,32 +1,51 @@
-const express = require('express');
-const session = require('express-session');
-const routes = require('./controllers');
+const path = require("path");
+const express = require("express");
+// const session = require("express-session");
+const exphbs = require("express-handlebars");
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const routes = require("./controllers");
+
+// const sequelize = require("./config/connection");
+// const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sess = {
-  secret: process.env.SECRET,
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
 
-app.use(session(sess));
+
+
+
+const hbs = exphbs.create({});
+
+// const sess = {
+//   secret: process.env.SECRET,
+//   cookie: {},
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize,
+//   }),
+// };
+
+// app.use(session(sess));
+
+
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(require("./controllers/Opener"));
 
-app.use(routes);
 
 
-
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log("Now listening"));
+// });
+app.listen(PORT, () => {
+  console.log("Server listening on: http://localhost:" + PORT);
 });
