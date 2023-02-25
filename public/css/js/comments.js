07bd4f215ae28cfe597a7f09689a1353f6029530
LucasBielinski <115428815+LucasBielinski@users.noreply.github.com>
2023-02-24 19:32:26 -0500

    add templates for frotnend

---
+++ public/css/js/comments.js
@@ -0,0 +1,23 @@
+const commentHandler = async function (event) {
+  event.preventDeafult();
+  const petId = document.querySelector().value;
+  const body = document.querySelector().value;
+
+  if (body) {
+    await fetch("/api/comment", {
+      method: "POST",
+      body: JSON.stringify({
+        petId,
+        body,
+      }),
+      headers: {
+        "Content-Type": "application/jason",
+      },
+    });
+    document.location.reload();
+  }
+};
+
+document
+  .querySelector("#new-comment-form")
+  .addEventListener("submit", commentHandler);
