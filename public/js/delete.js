f641a647e140bab6d639642f94a644a90648a9ce
ScrimmyFS <dscrimshaw12345@gmail.com>
2023-02-25 15:50:40 -0500

    Added css styling

--- public/js/delete.js
+++ public/js/delete.js
@@ -3,10 +3,13 @@ const id = document.getElementById("petone").value;
 console.log(id);
 
 const deleteHandle = async function () {
+
+  console.log(id);
+  
   await fetch(`/api/pet/${id}`, {
     method: "DELETE",
   });
-  document.location.replace;
+  document.location.replace('/profile');
 };
 
 document.querySelector("#deleteme").addEventListener("click", deleteHandle);
--- server.js
+++ server.js
@@ -19,7 +19,12 @@ const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: process.env.SECRET,
-  cookie: {},
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict"
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -39,6 +44,7 @@ app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+
 app.use(routes);
 
 
